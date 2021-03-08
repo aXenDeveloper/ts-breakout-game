cb2845bf654a78d9c5c529302b702c77db712c84
Maciej aXenDev Balcerzak <ithereplay@gmail.com>
2021-03-08 21:48:45 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# (TypeScript) Breakout Game
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './build'
+  }
+};
